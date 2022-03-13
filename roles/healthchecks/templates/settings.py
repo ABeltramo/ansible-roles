148fd4d755450dbc282e95f065e55b10afe64523
ABeltramo <beltramo.ale@gmail.com>
2022-03-13 20:28:01 +0000

    fixes: mariadb and table management

--- roles/healthchecks/templates/settings.py
+++ roles/healthchecks/templates/settings.py
@@ -1,27 +1,28 @@
 EMAIL_HOST = "{{ mail.server }}"
-DEFAULT_FROM_EMAIL = "{{ machine_name }}@{{ email_from }}"
+DEFAULT_FROM_EMAIL = "{{ machine_name }}@{{ mail.default_from }}"
 EMAIL_PORT = "{{ mail.port }}"
 EMAIL_HOST_PASSWORD = "{{ mail.password }}"
 EMAIL_HOST_USER = "{{ mail.user }}"
 EMAIL_USE_TLS = True
 
 BASE_URL = "{{ traefik.url }}"
-SITE_ROOT = "{{ site_root }}"
+SITE_ROOT = "http://{{ traefik.url }}"
 DEBUG = False
-SITE_NAME = "{{ site_name }}"
+SITE_NAME = "{{ site_name | default('Healthchecks') }}"
 ALLOWED_HOSTS = ["{{ traefik.url }}"]
-CSRF_TRUSTED_ORIGINS = ["{{ traefik.url }}"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://{{ traefik.url }}"]  # As of Django 4.0, the values in the CSRF_TRUSTED_ORIGINS setting must start with a scheme (usually http:// or https://) but found checks.lan
 PING_ENDPOINT = SITE_ROOT + "/ping/"
 
 # SEE https://github.com/linuxserver/docker-healthchecks/issues/37
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
-#         'HOST': "{{sql_host}}",
-#         'PORT': "{{sql_port}}",
-#         'NAME': "{{sql_db}}",
-#         'USER': "{{sql_user}}",
-#         'PASSWORD': "{{sql_password}}",
+#         'HOST': "{{SQL.host}}",
+#         'PORT': "{{SQL.port | default('3306')}}",
+#         'NAME': "{{SQL.db}}",
+#         'USER': "{{SQL.user}}",
+#         'PASSWORD': "{{SQL.password}}",
 #         'TEST': {'CHARSET': 'UTF8'}
 #     }
 # }
